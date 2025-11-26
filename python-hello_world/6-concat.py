581d83139a99d11297f76020418e3fc2838f1fce
rateelbahathiq <rateelbahathiq@gmail.com>
2025-11-26 07:21:20 +0000

    update 6-concat.py

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+# comment from the template MUST be here
 print(f"Welcome to {str1} {str2}!")
