14b7826c88868ae94c85f1f3f3fd114e065ca5aa
rateelbahathiq <rateelbahathiq@gmail.com>
2025-12-04 11:29:41 +0000

    fix spaces

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]): 
-    NewMatrix = [] 
-    for row in matrix:  
-        Newrow = list(map(lambda x: x*x, row))
+def square_matrix_simple(matrix=[]):
+    """Return a new matrix with each value squared."""
+    NewMatrix = []
+    for row in matrix:
+        Newrow = list(map(lambda x: x * x, row))
         NewMatrix.append(Newrow)
-
     return NewMatrix
