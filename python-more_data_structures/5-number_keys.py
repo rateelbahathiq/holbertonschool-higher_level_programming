63bd38ce93765b7a841a25f9d5b8a6f04d1b5707
rateelbahathiq <rateelbahathiq@gmail.com>
2025-12-05 19:41:39 +0000

    fix task 11 and add task 8,7,5

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """Return the number of keys in a dictionary."""
+    if a_dictionary is None:
+        return 0
+    return len(a_dictionary)
