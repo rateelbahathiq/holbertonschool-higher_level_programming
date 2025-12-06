208b9a35ecdb4ff4b58577f359f09a6086b54953
rateelbahathiq <rateelbahathiq@gmail.com>
2025-12-06 15:35:13 +0000

    add task 7,8

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    """Return tuple with length of string and first character."""
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
