9e91203e9bdc815bb9b4a3125e49278a2dc11b24
rateelbahathiq <rateelbahathiq@gmail.com>
2025-12-10 07:34:00 +0000

    add task 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define class Square. """
+
+
+class Square:
+    """Square class with size validation."""
+    
+    def __init__(self,size=0):
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
