4baf3c039ecf59b3e9dbf51ace4742d3c68662e4
rateelbahathiq <rateelbahathiq@gmail.com>
2025-12-10 07:51:09 +0000

    add task 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Define class Square."""
+
+
+class Square:
+    """Square class with size validation."""
+    
+    def __init__(self, size=0):
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the  area of the square."""
+        return self.__size * self.__size
