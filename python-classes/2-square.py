e8b2bf7d9f436544859426fa3fd5ecae4bbff1b5
rateelbahathiq <rateelbahathiq@gmail.com>
2025-12-10 07:35:28 +0000

    fix format

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -2,6 +2,7 @@
 """Define class Square. """
 
 
+
 class Square:
     """Square class with size validation."""
     
@@ -11,4 +12,3 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-
