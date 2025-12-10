d178db2c0f74e662c134b1f1c480380ebdd65406
rateelbahathiq <rateelbahathiq@gmail.com>
2025-12-10 07:10:08 +0000

    fix format

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Define a Square class """
+"""Define a Square class."""
+
 
 class Square:
-    """Add a private instance attribute called size"""
+    """Add a private instance attribute called size."""
     def __init__(self, size):
         self.__size = size
+
