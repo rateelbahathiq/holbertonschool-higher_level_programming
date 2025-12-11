2fbde335096a2e4c785cdd18dd1bbf57e0889a08
rateelbahathiq <rateelbahathiq@gmail.com>
2025-12-11 08:06:46 +0000

    fix spaces

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -59,4 +59,4 @@ class Rectangle:
 
     def __repr__(self):
         """Return a string that can recreate the rectangle."""
-        return "Rectangle({}, {})".format(self.__width, self.__height) 
+        return "Rectangle({}, {})".format(self.__width, self.__height)
