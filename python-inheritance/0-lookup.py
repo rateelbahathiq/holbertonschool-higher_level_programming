3100ea80264111935ffa02ffb6e59158eb0584a7
rateelbahathiq <rateelbahathiq@gmail.com>
2025-12-15 06:43:21 +0000

    fix module error

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Defines a function to list available attributes and methods of an object."""
+
+
 def lookup(obj):
     """Return the list of available attributes and methods of an object."""
     return dir(obj)
