ae52dfc64945bbc5cea2e3af060ebdd93df16168
root <rateelbahathiq@gmail.com>
2025-12-20 18:40:03 +0000

    Fix task 1 duck typing

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from abc import ABC, abstractmethod
 import math
 
@@ -7,36 +8,49 @@ class Shape(ABC):
 
     @abstractmethod
     def area(self):
-        pass
+        """Return the area of the shape."""
+        raise NotImplementedError
 
     @abstractmethod
     def perimeter(self):
-        pass
+        """Return the perimeter of the shape."""
+        raise NotImplementedError
 
 
 class Circle(Shape):
+    """Circle shape."""
+
     def __init__(self, radius):
         self.radius = radius
 
     def area(self):
+        """Return area of the circle."""
         return math.pi * (self.radius ** 2)
 
     def perimeter(self):
+        """Return perimeter (circumference) of the circle."""
         return 2 * math.pi * self.radius
 
 
 class Rectangle(Shape):
+    """Rectangle shape."""
+
     def __init__(self, width, height):
         self.width = width
         self.height = height
 
     def area(self):
+        """Return area of the rectangle."""
         return self.width * self.height
 
     def perimeter(self):
+        """Return perimeter of the rectangle."""
         return 2 * (self.width + self.height)
 
 
 def shape_info(shape):
-    print("Area: {}".format(shape.area()))
-    print("Perimeter: {}".format(shape.perimeter()))
+    """Print the area and perimeter of any shape-like object."""
+    area = shape.area()
+    perimeter = shape.perimeter()
+    print("Area: {}".format(area))
+    print("Perimeter: {}".format(perimeter))
