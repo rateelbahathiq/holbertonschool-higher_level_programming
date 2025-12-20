b9593e424e3be347db5a504cb4f99fead87f7940
root <rateelbahathiq@gmail.com>
2025-12-20 18:20:50 +0000

    add task

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 from abc import ABC, abstractmethod
 import math
 
@@ -8,47 +7,36 @@ class Shape(ABC):
 
     @abstractmethod
     def area(self):
-        """Return the area of the shape."""
         pass
 
     @abstractmethod
     def perimeter(self):
-        """Return the perimeter of the shape."""
         pass
 
 
 class Circle(Shape):
-    """Circle shape."""
-
     def __init__(self, radius):
         self.radius = radius
 
     def area(self):
-        """Return area of the circle."""
         return math.pi * (self.radius ** 2)
 
     def perimeter(self):
-        """Return perimeter (circumference) of the circle."""
         return 2 * math.pi * self.radius
 
 
 class Rectangle(Shape):
-    """Rectangle shape."""
-
     def __init__(self, width, height):
         self.width = width
         self.height = height
 
     def area(self):
-        """Return area of the rectangle."""
         return self.width * self.height
 
     def perimeter(self):
-        """Return perimeter of the rectangle."""
         return 2 * (self.width + self.height)
 
 
 def shape_info(shape):
-    """Print the area and perimeter of any shape-like object."""
-    print(f"Area: {shape.area()}")
-    print(f"Perimeter: {shape.perimeter()}")
+    print("Area: {}".format(shape.area()))
+    print("Perimeter: {}".format(shape.perimeter()))
