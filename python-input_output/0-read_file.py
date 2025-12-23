20cb262c7bcbfe36c56179e242e22a59b208af55
rateelbahathiq <rateelbahathiq@gmail.com>
2025-12-23 06:22:52 +0000

    fix error

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""Module that defines the read_file function.
+Reads a text file (UTF8) and prints its contents to stdout.
+"""
+
+
 def read_file(filename=""):
-    """Reads a UTF-8 text file and prints it to stdout"""
+    """Read a UTF-8 text file and print it to stdout."""
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
