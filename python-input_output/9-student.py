653e51962a35b7727eb59fdf428f6eca01d6a82d
rateelbahathiq <rateelbahathiq@gmail.com>
2025-12-23 08:21:02 +0000

    add task 9

---
+++ python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module that defines the Student class."""
+      
+
+class Student:
+    """Class that defines a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize student."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Return dictionary representation of the instance."""
+        return self.__dict__
