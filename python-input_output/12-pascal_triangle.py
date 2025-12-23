a22a874d395f60d71ce0baff1eca545309c43a90
rateelbahathiq <rateelbahathiq@gmail.com>
2025-12-23 08:32:14 +0000

    add task 12

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module that returns Pascal's Triangle"""
+
+
+def pascal_triangle(n):
+    """Return a list of lists representing Pascal's triangle of n"""
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        prev = triangle[-1]
+        row = [1]
+
+        for j in range(1, len(prev)):
+            row.append(prev[j - 1] + prev[j])
+
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
