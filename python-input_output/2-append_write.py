41c994d81cd90f00e34a42909f98bc2501608167
rateelbahathiq <rateelbahathiq@gmail.com>
2025-12-23 07:48:35 +0000

    fix format

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Module that appends text to a file."""
-      
+
 
 def append_write(filename="", text=""):
     """Append a string to a UTF-8 text file and return characters added."""
