07a86b8a64ed892e63db928f16fdf506e87726ea
rateelbahathiq <rateelbahathiq@gmail.com>
2026-01-19 07:42:08 +0000

    add task 15

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,9 @@
+-- Lists all TV shows that belong to the Comedy genre
+SELECT tv_shows.title
+FROM tv_genres
+INNER JOIN tv_show_genres
+  ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+  ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC;
