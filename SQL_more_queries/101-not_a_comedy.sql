46a36a35298476a5b69f9a4eedaf0a71c5339923
rateelbahathiq <rateelbahathiq@gmail.com>
2026-01-19 07:48:41 +0000

    add advance task

---
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,11 @@
+-- Lists all TV shows that are not linked to the Comedy genre
+SELECT tv_shows.title
+FROM tv_shows
+WHERE tv_shows.id NOT IN (
+    SELECT tv_show_genres.show_id
+    FROM tv_genres
+    INNER JOIN tv_show_genres
+      ON tv_genres.id = tv_show_genres.genre_id
+    WHERE tv_genres.name = 'Comedy'
+)
+ORDER BY tv_shows.title ASC;
